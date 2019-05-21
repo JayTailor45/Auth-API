d13d79617efb6263bc6e5a36ed2ebaf02b22feb7
Jay Tailor <tailorj64@gmail.com>
2019-05-21 14:34:55 +0530

    Setup Sequelize 😋

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const express = require('express');
-
+const Sequelize = require('sequelize');
 const app = express();
 
-const PORT = 3000;
+const PORT          = 3000;
+const DATABASE_NAME = 'user_db';
+const HOSTNAME      = 'localhost';
+const USERNAME      = 'root';
+const PASSWORD      = '';
+const DB_PORT       = 3333;
+
 
 //Initial configurations
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+//Initilize instance of Sequelize
+const sequelize = new Sequelize(DATABASE_NAME,USERNAME,PASSWORD,{
+    host    : HOSTNAME, 
+    dialect : 'mysql',
+    port    : DB_PORT
+});
 
 //First basic route
 app.get('/', (req,res) => {
     res.send({msg: 'Server is connected'}).status(200);
 });
 
+//Check database connection
+sequelize
+    .authenticate()
+    .then(()=> {
+        console.log('Database connection success')
+    })
+    .catch((err) => {
+        console.log('Database connection fail.' + err)
+    });
 
 //Fire server to listen on given port
 app.listen(PORT, () => {
